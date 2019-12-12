c78c1aa93acc577dad11ca3a5c4d63dd085195f1
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2019-12-12 19:42:58 +0100

    search method changed to GET

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<form method="POST" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form method="GET" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input class="seach-input" type="text" name="s" placeholder="<?php esc_html_e( 'Type words and hit enter', 'pierogi' ); ?>" value="<?php echo esc_attr( get_query_var( 's' ) ); ?>">
 	<input id="search-button" class="btn search-form-btn" type="submit" value="<?php esc_html_e( 'Search', 'pierogi' ); ?>" />
 	<button type="button" class="search-close">
