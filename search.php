935067a17bc077742aae64c87967cbf80dd10557
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2019-12-20 12:26:34 +0100

    search results fix

--- search.php
+++ search.php
@@ -33,30 +33,34 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+			<div class="posts">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				<?php if ( have_posts() ) : ?>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'loop' );
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-			endwhile;
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'loop' );
 
-			the_posts_pagination( [ 'mid_size' => 2 ] );
+					endwhile;
 
-		else :
+					the_posts_pagination( [ 'mid_size' => 2 ] );
 
-			get_template_part( 'template-parts/content', 'none' );
+				else :
 
-		endif;
-		?>
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+
+			</div>
 
 		</main><!-- #main -->
 
